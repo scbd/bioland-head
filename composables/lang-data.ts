08e9569a8e60572335cf88aeeacc8390a6806bca
Randy Houlahan <randy.houlahan@cbd.int>
2023-10-02 14:00:42 -0400

    BL-18 lang header component

---
+++ composables/lang-data.ts
@@ -0,0 +1,12 @@
+
+
+export const useLanguageMenus = async () => {
+    const siteIdentifier = useState('siteIdentifier');
+    const { baseHost, gaiaApi, drupalMultisiteIdentifier }   = useRuntimeConfig().public;
+
+    const uri = `${gaiaApi}v2023/drupal/multisite/${drupalMultisiteIdentifier}/${siteIdentifier.value}/locales`
+
+    const { data, error } = await useFetch(uri)
+
+    return data.value
+}
