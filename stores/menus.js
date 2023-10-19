812a86b6d44716b999e8e60e69d84af65eb08c72
Randy Houlahan <randy.houlahan@cbd.int>
2023-10-19 11:27:33 -0400

    BL-37 i18n reload drupal data on local change and cleaning

--- stores/menus.js
+++ stores/menus.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 
 
-const actions = { set }
+const actions = { set, loadAllMenus }
 
 export const useMenusStore = defineStore('menus', { state, actions })
 
@@ -21,4 +21,15 @@ function set(name, value){
     this.$patch({ [name]: unref(value) } );
 
     return this;
+}
+
+function loadAllMenus(){
+    const allPromises = [];
+
+    allPromises[0] = useFooterMenus().then((menus) => this.set('footer', menus) );
+    allPromises[1] = useMainMenus().then((menus) => this.set('main', menus));
+    allPromises[2] = useFooterCreditsMenus().then((menus) => this.set('footerCredits', menus));
+    allPromises[3] = useLanguageMenus().then((menus) => this.set('languages', menus));
+
+    return Promise.all(allPromises);
 }
--- stores/site.js
+++ stores/site.js
@@ -1,19 +1,19 @@
 import { defineStore } from 'pinia'
+import { getBiolandSiteIdentifier } from "~/util";
 
-
-const actions = { set }
+const actions = { set, initialize, getSiteDefaultLocale, watchLocaleChange }
 
 export const useSiteStore = defineStore('site', { state, actions })
 
 const initState = { 
-    locale: undefined,
-    identifier: undefined,
-    pagePath: undefined,
-    defaultLocale: undefined,
-    gaiaApi: undefined,
-    drupalMultisiteIdentifier: undefined,
-    baseHost: undefined,
-}
+                    locale                   : undefined,
+                    identifier               : undefined,
+                    pagePath                 : undefined,
+                    defaultLocale            : undefined,
+                    gaiaApi                  : undefined,
+                    drupalMultisiteIdentifier: undefined,
+                    baseHost                 : undefined,
+                }
 
 function state(){ return initState }
 
@@ -28,3 +28,38 @@ function set(name, value){
     return this;
 }
 
+async function initialize(nuxtApp){
+    const { gaiaApi, drupalMultisiteIdentifier, baseHost }   = useRuntimeConfig().public;
+    const { hostname } = useRequestURL();
+
+    this.set('baseHost',baseHost);
+    this.set('gaiaApi',gaiaApi);
+    this.set('drupalMultisiteIdentifier',drupalMultisiteIdentifier);
+    this.set('locale',nuxtApp.$i18n.locale);
+    this.set('identifier',getBiolandSiteIdentifier(hostname) || 'seed');
+    this.set('defaultLocale',await this.getSiteDefaultLocale());
+}
+
+function watchLocaleChange(nuxtApp, functions = []){
+    const locale  =  nuxtApp.$i18n.locale
+
+    watch(locale, async (newLocale) => {
+        this.set('locale',newLocale);
+        await this.initialize(nuxtApp);
+
+        for (const aFunction of functions)
+            await aFunction();
+        
+    }, { immediate: true });
+}
+
+async function getSiteDefaultLocale(){
+    const { identifier, gaiaApi, drupalMultisiteIdentifier } = this;
+
+
+    const uri = `${gaiaApi}v2023/drupal/multisite/${drupalMultisiteIdentifier}/${identifier}/default-locale`
+
+    const { data, error } = await useFetch(uri)
+
+    return data.value
+}
