d7963d7b6832cd1bdf8dcbeaee89622ac5f7d8bf
Randy Houlahan <randy.houlahan@cbd.int>
2023-10-20 11:58:27 -0400

    BL-19 save hero version

--- components/page/header/index.vue
+++ components/page/header/index.vue
@@ -6,9 +6,13 @@
         <!-- UN Logo Header, Search Bar, and Menu Bar based on UN Web Styleguide -->
         <div class="navbar navbar-dark">
             <div class="container">
-                <div class="db text-muted">Desktop Header <a href="https://scbd.atlassian.net/browse/BL-19" target="_blank">BL-19</a></div>
+                <PageHeaderTitleSearch/> 
+
+
+            </div>
+            <div class="container-fluid">
                 <div class="db text-muted">Mega Menu <a href="https://scbd.atlassian.net/browse/BL-16" target="_blank">BL-16</a></div>
-                <!-- <PageHeaderTitleSearch/>    -->
+
                 <!-- <PageHeaderMegaMenu/>   -->
             </div>
         </div> 
