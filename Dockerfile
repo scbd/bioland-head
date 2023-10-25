e201a2bb644c2436361689f8bb3313824231e5e7
Randy Houlahan <randy.houlahan@cbd.int>
2023-10-25 14:19:49 -0400

    BL-41 optimizations

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
-FROM node:18-alpine
+FROM node:18
 
-RUN apk update && apk upgrade && \
-    apk add --no-cache yarn && \
-    apk add --no-cache curl
+RUN apt update && \
+    apt install yarn curl -y
 
 WORKDIR /usr/src/app
 
 COPY package.json ./
 
 RUN yarn clean-reinstall
+RUN npm install --platform=linux --arch=64x --arm-version=7 --libc=glibc sharp
 
 COPY . ./
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,15 +28,18 @@ export default defineNuxtConfig({
   modules: [
     'nuxt-viewport',
     '@nuxtjs/i18n-edge',
-    '@pinia/nuxt'
+    '@pinia/nuxt',
+    '@nuxt/image',
+    'nuxt-delay-hydration',
+    'nuxt-purgecss'
   ],
   viewport: {
     breakpoints: {
-      xs: 300,
+      xs: 1,
       sm: 576,
       md: 768,
       lg: 992,
-      xl: 1200,
+      xl: 1250,
       xxl: 1400
     },
 
@@ -72,8 +75,7 @@ export default defineNuxtConfig({
     // vueI18n:'./config/i18n.config.ts',
   },
   pinia: { autoImports: [ 'defineStore','useStore', 'storeToRefs'], },
-    vite: {
-    
+  vite: {
       resolve: {
           alias: {
               'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js'
@@ -82,6 +84,27 @@ export default defineNuxtConfig({
       plugins: [
           viteSyncI18nFiles()
       ]
+  },
+  delayHydration: {
+    mode: 'init',
+    // enables nuxt-delay-hydration in dev mode for testing
+    debug: process.env.NODE_ENV === 'development'
+  },
+  purgecss: {
+    enabled: process.env.NODE_ENV !== 'development', // Always enable purgecss
+    safelist: ['my-class'], // Add my-class token to the safelist (e.g. .my-class)
+  },
+  image: {
+    domains: ['https://seed.chm-cbd.net', 'https://www.cbd.int', 'https://seed.bl2.staging.cbd.int'],
+    format: ['webp', 'avif', 'jpeg', 'jpg', 'png','gif'],
+    screens: {
+      'xs': 320,
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+      xxl: 1400,
+      '2xl': 1400
+    },
   }
-
 })
