0c9eb513fcb444f8f794dc1c5d4b85c5a9b439f8
Randy Houlahan <randy.houlahan@cbd.int>
2023-10-29 13:19:16 -0400

    save

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-    <IconsSymbols v-once/>
+    <IconSymbols v-once/>
+    <NuxtLoadingIndicator />
     <PageHeader/>
     <!-- <PageEditorControls  /> -->
     <main class="debug-main">
