58feb371c2ee8ab9e35c042c6b011c373daf16e3
Randy Houlahan <randy.houlahan@cbd.int>
2023-12-05 12:15:30 -0500

    save

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -100,11 +100,11 @@ export default defineNuxtConfig({
   },
   pinia: { autoImports: [ 'defineStore','useStore', 'storeToRefs'], },
   vite: {
-      resolve: {
-          alias: {
-              'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js'
-          }
-      },
+      // resolve: {
+      //     alias: {
+      //         'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js'
+      //     }
+      // },
       plugins: [
           viteSyncI18nFiles()
       ]
