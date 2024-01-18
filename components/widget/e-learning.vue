2cb148b86ed26d02cc4e6aa7dbd9ce01734db763
Randy Houlahan <randy.houlahan@cbd.int>
2024-01-18 14:46:34 -0500

    save

--- components/widget/e-learning.vue
+++ components/widget/e-learning.vue
@@ -1,3 +1,28 @@
 <template>
-    <NuxtImg src="/w3.png" class="w-100"/>
+    <Widget v-if="record" name="e-Learning" :record="record" :links="links"/>
 </template>
+<script setup>
+
+    import { useSiteStore } from '~/stores/site' ;
+
+    const siteStore  = useSiteStore();
+    const query      = { ...siteStore.params };
+    const localePath = useLocalePath();
+
+
+    const { data: record  }= await useFetch(`/api/list/content/4`, {  method: 'GET', query, onResponse });
+
+
+    function onResponse({ request, response, options}){
+        const { data }   = response._data;
+        const { length } = data || []
+
+
+        response._data = data[Math.floor(Math.random() * length)];
+    }
+    
+    const links = [
+        { name: 'Browse Courses',  to: { path:localePath('/search'),query:{ schemas:[4,4]}}},
+    ];
+</script>
+
