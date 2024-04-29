6c5890846082abddb5f75a3270a849009de783b1
Randy Houlahan <randy.houlahan@cbd.int>
2024-04-29 10:30:15 -0400

    save

---
+++ server/api/me.get.js
@@ -0,0 +1,67 @@
+export default defineEventHandler(async (event) => {
+    try{
+
+        const user = await getUser(event);
+
+        
+        return user
+    }
+    catch(e){
+        console.error(e);
+        throw createError({
+            statusCode: 500,
+            statusMessage: 'Failed to get me me',
+        }); 
+    }
+    
+})
+const anonUser = { userID: 1, name: 'anonymous', email: '@anonymous', isAuthenticated: false, roles: [] }
+
+async function getUser(event) {
+
+    const { host } = getContext(event);
+
+    const uri           = `${host}/jsonapi`;
+    const method        = 'get';
+    const headers       = { 'Content-Type': 'application/json', Cookie: 'SSESScdf9bf2e11fb28fdb7542fa5e770c587=1X7vIKKIWOjg94wlHNUYJqj-DwYlEL9LnSYoLZ9ItlOF8yNl;'};//getHeader(event, 'Cookie')
+
+
+    const {  meta:m } = await $fetch(uri, { method, headers });
+
+
+    if(!m?.links?.me) return anonUser
+
+    if(!m?.links?.me?.href) throw new Error('/api/me/getUer: no href property on ');
+
+    const userUri = `${m.links.me.href}?include=roles`;
+    const t= await $fetch(userUri, { method, headers });
+
+    return  mapUserFromDrupal(t)
+};
+
+function hasSessionCookie(event){
+    const cookies = parseCookies(event);
+
+    const [hasDrupalSession] = Object.keys(cookies).filter(key => key.startsWith('SSESS'))|| [false];
+
+    return hasDrupalSession;
+}
+
+function mapUserFromDrupal({ data, included }){
+    const { id, attributes } = data;
+
+    return {
+        duuid          : id,
+        diuid          : attributes?.drupal_internal__uid,
+        preferredLang  : attributes?.preferred_langcode,
+        display_name   : attributes?.display_name,
+        name           : attributes?.name,
+        email          : attributes?.mail,
+        isAuthenticated: true,
+        roles          : mapRolesFromDrupal(included)
+    }
+}
+
+function mapRolesFromDrupal(included){
+    return included.filter(({ type }) => type === 'user_role--user_role').map(({ attributes }) => (attributes?.drupal_internal__id))
+}
