222cfd334e065e63adfe122ec3c41148e7ddfe1d
Randy Houlahan <randy.houlahan@cbd.int>
2025-03-12 13:02:59 -0400

    BL-347 movie loading issue

--- utils/html.js
+++ utils/html.js
@@ -1,6 +1,17 @@
 import DOMPurify from "isomorphic-dompurify";
 
-const defaultOptions = { USE_PROFILES: { html: true } };
+const defaultOptions = { USE_PROFILES: { html: true },ADD_TAGS: ["iframe"], ADD_ATTR: ['allow', 'allowfullscreen', 'frameborder', 'scrolling'] };
 
 export const htmlSanitize = (html, options = {}) => html? DOMPurify.sanitize(html, { ...defaultOptions, ...options } ) : '';
 
+DOMPurify.addHook('uponSanitizeElement', (node, data)=>
+  {
+    if (data.tagName !== 'iframe') return node;
+
+    if(node.getAttribute("src")?.includes('youtube.com')) return node;
+    if(node.getAttribute("src")?.startsWith('https://portal.geobon.org')) return node;
+
+
+    return node.parentNode.parentNode.removeChild(node.parentNode);//node;
+  });
+  
