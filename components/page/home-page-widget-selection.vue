3d1818fe486e774904f6b4e50c5b1d1813dfb6c9
Randy Houlahan <randy.houlahan@cbd.int>
2025-04-04 14:12:25 -0400

    BL-463 - turn forums on and off, also with other home page widgets

---
+++ components/page/home-page-widget-selection.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <LazySwiperNewsUpdates      v-if="isNews"/>
+        <LazyWidgetPanorama         v-if="isPanorama"/>
+        <LazyWidgetGbif             v-if="isGbif"/>
+        <LazyWidgetELearning        v-if="isELearning"/>
+        <LazyWidgetImplementation   v-if="isImplementation"/>
+        <LazyWidgetTsc              v-if="isTsc"/>
+        <LazyWidgetForums           v-if="isForums"/>
+        <LazyWidgetGeobon           v-if="isGeobon"/>
+    </div>
+</template>
+<script setup>
+const props  = defineProps({ is: { type: String, required:true } });
+const { is } = toRefs(props);  
+
+const isNews           = computed(()=> is.value === 'news'           || is.value?.includes('SwiperNewsUpdates'));
+const isPanorama       = computed(()=> is.value === 'panorama'       || is.value?.includes('WidgetPanorama'));
+const isGbif           = computed(()=> is.value === 'gbif'           || is.value?.includes('WidgetGbif'));
+const isELearning      = computed(()=> is.value === 'eLearning'      || is.value?.includes('WidgetELearning'));
+const isImplementation = computed(()=> is.value === 'implementation' || is.value?.includes('WidgetImplementation'));
+const isTsc            = computed(()=> is.value === 'tsc'            || is.value?.includes('WidgetTsc'));
+const isForums         = computed(()=> is.value === 'forums'         || is.value?.includes('WidgetForums'));
+const isGeobon         = computed(()=> is.value === 'geobon'         || is.value?.includes('WidgetGeobon'));
+</script>
