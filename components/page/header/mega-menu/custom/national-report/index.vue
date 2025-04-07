6933fdb642b684b6c1581fef2629be05f0cf08de
Randy Houlahan <randy.houlahan@cbd.int>
2025-04-07 12:05:04 -0400

    BL-433 - mega menu closing funny for login

--- components/page/header/mega-menu/custom/national-report/index.vue
+++ components/page/header/mega-menu/custom/national-report/index.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="col-12 text-wrap px-0">
-        <LazyPageHeaderMegaMenuCustomNationalReportList v-if="!showNrSix"/>
-        <LazyPageHeaderMegaMenuCustomNationalReportSix v-if="showNrSix"/>
+        <LazyPageHeaderMegaMenuCustomNationalReportList v-if="!showNrSix" :menu="passedMenu"/>
+        <LazyPageHeaderMegaMenuCustomNationalReportSix v-if="showNrSix" :menu="passedMenu"/>
 
     </div>
 </template>
 
 <script setup >
+
+
     const menusStore   = useMenusStore();
     const siteStore    = useSiteStore();
+    const   props              = defineProps({ menu: Object });
+    const { menu: passedMenu } = toRefs(props);
+
+
     const menus        = computed(() => menusStore.nrSix);
     const countries    = computed(() => Object.keys(menus.value));
     const nrSixData    = computed(() => unref(menus)[siteStore.config.country] || []);
