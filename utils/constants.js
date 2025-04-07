9ad50f03550ddd8d8f9564127798064544c4bcd5
Randy Houlahan <randy.houlahan@cbd.int>
2025-04-07 12:52:51 -0400

    BL-448 - global consts

---
+++ utils/constants.js
@@ -0,0 +1,13 @@
+export const systemPageTidConstants = {
+    HOME: 20,
+    SEARCH: 21,  
+    NEWS: 22,
+    NATIONAL_CONTACT_POINTS: 30,
+    FORUMS: 24,
+    CREDITS: 25,
+    TERMS_OF_USE: 26,
+    BL2_MIGRATION: 35,
+    CHM_NETWORK:38,
+    SITE_MAP_HTML: 39,
+    SITE_MAP_XML: 40,
+}
