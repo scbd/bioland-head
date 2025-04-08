4f67de4845fe09d1114d046c48438236e3e6d1f1
RJH <randy.houlahan@cbd.int>
2025-04-08 15:11:25 -0400

    Update components/page/body/index.vue
    Co-authored-by: Stephane Bilodeau <bilodeaustephane@gmail.com>

--- components/page/body/index.vue
+++ components/page/body/index.vue
@@ -86,7 +86,7 @@
 
             <div v-if="pageStore?.tags?.gbfTargets?.length" class="row mt-3">
                 <div class="col-12 col-md-3">
-                    <h2 :style="pageTypeStyle" class="side-heading text-nowrap">{{t('GBF Targets')}} <span class="text-muted fs-4">({{pageStore?.tags.gbfTargets.length}})</span></h2>
+                    <h2 :style="pageTypeStyle" class="side-heading text-nowrap">{{t('GBF Targets')}} <span class="text-muted fs-4">({{pageStore?.tags?.gbfTargets?.length}})</span></h2>
                 </div>
                 <div class="col-12 col-md-9">
                     <LazySwiperGbf :slides="pageStore?.tags?.gbfTargets" type="gbf"/>
