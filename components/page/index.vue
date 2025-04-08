35ac290161b0943c08f0256c3ede8afd0fc00fbb
RJH <randy.houlahan@cbd.int>
2025-04-08 15:54:15 -0400

    BL-439- cleaning (#58)
    * BL-439 cleaning
    
    * BL-439 - cleaning
    
    * BL-439 cleaning

--- components/page/index.vue
+++ components/page/index.vue
@@ -1,13 +1,18 @@
 <template>
-    <LazyPageBody              v-if="pageStore.isPage"/>
-    <LazyPageBodyMedia         v-if="pageStore.isMediaPage "/>  
-    <LazyPageList              v-if="pageStore.isSearch"/>
-    <LazyPageListForums        v-if="pageStore.isForumsList"/>
-    <LazyPageListTopics        v-if="pageStore.isTopicsList" />
-    <LazyPageListTopicComments v-if="pageStore.isTopicsCommentsList" /> 
-    <LazyPageListNcps          v-if="pageStore.isNcpsList" />
+    <section>
+        <LazyPageBody              v-if="pageStore.isPage"/>
+        <LazyPageBodyMedia         v-if="pageStore.isMediaPage "/>  
+        <LazyPageList              v-if="pageStore.isSearch"/>
+        <LazyPageListForums        v-if="pageStore.isForumsList"/>
+        <LazyPageListTopics        v-if="pageStore.isTopicsList" />
+        <LazyPageListTopicComments v-if="pageStore.isTopicsCommentsList" /> 
+        <LazyPageListNcps          v-if="pageStore.isNcpsList" />
+        <LazyPageCustomChmNetwork  v-if="pageStore.isChmNetwork" />
+    </section>
 </template>
 
 <script setup>
+
+
     const pageStore = usePageStore();
 </script>
