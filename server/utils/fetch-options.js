e05ab040d2e74857055531d5e44b01fdaa25b0db
Randy Houlahan <randy.houlahan@cbd.int>
2025-04-08 11:36:02 -0400

    BL-435 - performance updates and logging

--- server/utils/fetch-options.js
+++ server/utils/fetch-options.js
@@ -4,18 +4,22 @@ import { colors } from "consola/utils";
 export const $fetchBaseOptions = (options = {}) => ({onRequest, onRequestError,onResponse,onResponseError,method:'GET',redirect: 'follow',  ...options})
 
 async function onRequest({ request, options }) {
+  const { logAll, logServerOutRequests }  = useRuntimeConfig().public;
 
-  // consola.debug(`${colors.magentaBright('[fetch request]')}`, request);
+  if(logAll || logServerOutRequests)
+    consola.info(`${colors.magentaBright('[fetch request]')}`, request);
 
 }
 async function onRequestError({ request, options, error }) {
-  
     consola.error( `${colors.red('[fetch request error]')}`, request, error);
 }
 
 
 async function onResponse({ request, response, options }) {
-//  consola.debug(`${colors.cyan('[fetch response]')}`, request, `${colors.green(response.status)}`);
+  const { logAll, logServerOutRequests }  = useRuntimeConfig().public;
+
+  if(logAll || logServerOutRequests)
+    consola.info(`${colors.cyan('[fetch response]')}`, request, `${colors.green(response.status)}`);
 }
 
 async function onResponseError({ request, response, options }) {
