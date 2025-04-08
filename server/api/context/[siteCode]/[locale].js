c001705cbe12d2851d09126b8bda3af51121c4e6
Randy Houlahan <randy.houlahan@cbd.int>
2025-04-08 11:47:07 -0400

    BL-439 cleaning

--- server/api/context/[siteCode]/[locale].js
+++ server/api/context/[siteCode]/[locale].js
@@ -8,23 +8,22 @@ export default defineEventHandler(async (event) => {
             const host       = getRequestHeader(event, 'x-forwarded-host') || getRequestHeader(event, 'host');
             
             if(!siteCode) throw createError({ statusCode: 404, message: `Site code not found in request`, statusMessage:'Not Found' });
+
             if(!config?.locales?.includes(locale) && locale !== config?.defaultLocale)
                 throw createError({ statusCode: 404, message: `Locale [${locale}] not found in site [${siteCode}] config`, statusMessage:'Not Found' });
 
             const siteName = await getSiteDefinedName({ ...ctx, locale, config });
 
-            await cleanStorage();
-            const respCtx = { ...ctx, locale, config, siteName, defaultLocale: config.defaultLocale };
+            const respCtx = { ...ctx, locale, config, siteName, defaultLocale: config.defaultLocale, host };
 
-            await useStorage('context').setItem(host, respCtx);
+
             return  respCtx;
 
-            async function cleanStorage(){
-                const keys = await useStorage('db').getKeys();
-
-                for(let key of keys)
-                    if(key.includes('nitro:handlers:_:undefined'))
-                        await useStorage('db').removeItem(key);
+            function isValidLocale(locale){
+                const { locales } = useRuntimeConfig().public;
+                const   languages = locales.map(l => l.language);
+            
+                return languages.includes(locale);
             }
         }
         catch (e) {
@@ -32,10 +31,3 @@ export default defineEventHandler(async (event) => {
         }
     }
 );
-
-function isValidLocale(locale){
-    const { locales } = useRuntimeConfig().public;
-    const   languages = locales.map(l => l.language);
-
-    return languages.includes(locale);
-}
